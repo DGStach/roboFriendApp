064c4bbf3755136bfc7b1b2c9b9da66a706e684a
dagmara <dagmara.gabriela.stach@gmail.com>
2023-03-04 17:39:09 +0100

    image src error solve - image are fit for user id

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
     const {id, name, email} = props;
     return(
         <div className='tc bg-light-green  dib br3 pa3 ma2 grow bw2 shadow-5 '>
-            <img alt='photoRoboFriends' src={"https://robohash.org/${id}?200x200"} />
+            <img alt='robots' src={`https://robohash.org/${id}?200x200`}/>
             <div>
                 <p>{name}</p>
                 <h2>{email}</h2>
